1920d399b2f5d07a097905fdf54a1b5ac4942589
user-komeda <shigoto922@gmail.com>
2025-11-15 14:44:28 +0900

    solid導入 #5

--- apps/backend/config.ru
+++ apps/backend/config.ru
@@ -1,12 +1,10 @@
-﻿# frozen_string_literal: true
+# frozen_string_literal: true
 
 require_relative 'config/auto_load'
 require_relative 'config/router'
 require_relative 'config/boot/rom'
 require_relative 'config/boot/persistence'
 require 'dotenv/load'
-
-
 
 Container.finalize!
 run Main
---
+++ apps/backend/config/sinatra_settings.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'rack/protection'
+
+module SinatraSettings
+  def self.registered(app)
+    app.set :environment, :production
+    app.set :show_exceptions, false
+    app.set :raise_errors, false
+    app.set :dump_errors, true
+    app.set :logging, true
+    app.set :static_cache_control, [:public, { max_age: 3600 }]
+
+    # デフォルトで十分な保護
+    app.use Rack::Protection
+
+    # CORS
+    app.before do
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, PATCH, DELETE, OPTIONS'
+      headers['Access-Control-Allow-Headers'] = 'Content-Type, Accept, Authorization, X-Requested-With'
+      headers['Access-Control-Expose-Headers'] = 'Location, ETag'
+    end
+  end
+end
