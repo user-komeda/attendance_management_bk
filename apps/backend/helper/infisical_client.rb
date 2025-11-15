9ec9c383f7e322db4a42c0d3f467155b7212deb1
user-komeda <shigoto922@gmail.com>
2025-11-15 17:44:09 +0900

    solid導入 #5

--- apps/backend/helper/infisical_client.rb
+++ apps/backend/helper/infisical_client.rb
@@ -11,8 +11,8 @@ module InfisicalClient
   def self.client
     @client ||= InfisicalSDK::InfisicalClient.new('https://app.infisical.com').tap do |client|
       client.auth.universal_auth(
-        client_id: ENV['CLIENT_ID'] || '',
-        client_secret: ENV['CLIENT_SECRET'] || ''
+        client_id: ENV['CLIENT_ID'] || 'bcb5e70a-bce3-47b8-b176-15475e005f39',
+        client_secret: ENV['CLIENT_SECRET'] || 'b7d54d66dde7db57c76a59c2e926d168efce7a569446546a3fd5c049689eb77a'
       )
     end
   end
